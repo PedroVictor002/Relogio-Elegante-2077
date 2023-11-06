f3088dd0bf6a4043e6f7a8dcb9a4a73808da6467
Pedro Victor Castanheiro de Souza <71782584+PedroVictor002@users.noreply.github.com>
2023-11-06 19:51:42 -0300

    Finalizado

--- Java script/javscript.js
+++ Java script/javscript.js
@@ -4,21 +4,21 @@ const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
 const relogio = setInterval(function time(){
-let dateToday = new date();
+let dateToday = new Date();
 let hr = dateToday.getHours();
 let min = dateToday.getMinutes();
 let s = dateToday.getSeconds();
 
-if (hr < 10) hr + '0' + hr;
+if (hr < 10) hr = '0' + hr;
 
-if (min < 10) min + '0' + min;
+if (min < 10) min = '0' + min;
 
-if (s < 10) s + '0' + s;
+if (s < 10) s = '0' + s;
 
 
 horas.textContent = hr;
-minutes.textContent = min;
-seconds.textContent = s;
+minutos.textContent = min;
+segundos.textContent = s;
+
 })
 
-console.log("bom dia")
